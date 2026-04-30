2f7c4d6c0b087302b1fcddc2e9d20b186690d27e
Adhika P. <40765693+adhika16@users.noreply.github.com>
2026-04-30 21:34:19 +0700

    Add required meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta name="dicoding:email" content="villaadhika@gmail.com" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
